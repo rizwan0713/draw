3eb2e530ae6c3c70a14c15fbe572ece855bb53af
rizwan0713 <rizwansalmanirsa@gmail.com>
2025-07-10 02:37:13 +0530

    reStructured

--- apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
+++ apps/excelidraw-frontend/app/canvas/[roomId]/page.tsx
@@ -1,52 +1,22 @@
 "use client"
+import { initDraw } from "@/draw";
 import { useEffect, useRef } from "react"
 
 export default function   Canvas(){
     const canvasRef = useRef<HTMLCanvasElement>(null);
 
     useEffect(() => {
+
         if(canvasRef.current){
-            const canvas = canvasRef.current;
-            const ctx = canvas.getContext("2d");
-            if(!ctx){
-                return
-            }
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
-
-            canvas.addEventListener("mousedown" , (e) => {
-             clicked = true
-             console.log("down down dowan")
-             startX = e.clientX
-             startY = e.clientY
-            })
-
-            canvas.addEventListener("mouseup" , (e) => {
-             clicked = false
-             console.log("up up up")
-
-            //  startX = e.clientX
-            //  startY= e.clientY
-            })
-
-             canvas.addEventListener("mousemove" , (e) => {
-              
-                if(clicked){
-                    const width = e.clientX  - startX;
-                    const height = e.clientY - startY;
-                    ctx.clearRect(0,0,canvas.width , canvas.height);
-                    ctx.strokeRect(startX,startY,width,height)
-                }
-            })
             
+            initDraw(canvasRef.current)
 
         }
 
     },[canvasRef])
     return <div>
         <h1>this is canvas</h1>
-  <canvas ref={canvasRef} className="bg-white" width={500} height={500}></canvas>
+  <canvas ref={canvasRef}  width={2000} height={1000}></canvas>
     </div>
 }
 
