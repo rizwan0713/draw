aee58348ad4f0942b10ac66bd0fd994112818386
rizwan0713 <rizwansalmanirsa@gmail.com>
2025-07-10 03:27:02 +0530

    added holding logic of the shapes on the canvas

--- apps/excelidraw-frontend/draw/index.ts
+++ apps/excelidraw-frontend/draw/index.ts
@@ -1,42 +1,78 @@
-export function initDraw(canvas:HTMLCanvasElement){
-             const ctx = canvas.getContext("2d")
-            if(!ctx){
-                return
-            }
-            ctx.fillStyle ="rgba(0,0,0)";
-            ctx.fillRect(0,0, canvas.width,canvas.height);
-            let clicked = false;
-            let startX = 0;
-            let startY = 0;
+type Shape =
+  | {
+      type: "rect";
+      x: number;
+      y: number;
+      width: number;
+      height: number;
+    }
+  | {
+      type: "circle";
+      centerX: number;
+      centerY: number;
+      radius: number;
+    };
 
-            canvas.addEventListener("mousedown" , (e) => {
-             clicked = true
-             console.log("down down dowan")
-             startX = e.clientX
-             console.log("startX",startX)
-             startY = e.clientY
-             console.log("startY",startY)
+export function initDraw(canvas: HTMLCanvasElement) {
+  const ctx = canvas.getContext("2d");
+  let existingShapes: Shape[] = [];
+  if (!ctx) {
+    return;
+  }
+  ctx.fillStyle = "rgba(0,0,0)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  let clicked = false;
+  let startX = 0;
+  let startY = 0;
 
-            })
+  canvas.addEventListener("mousedown", (e) => {
+    clicked = true;
+    console.log("down down dowan");
+    startX = e.clientX;
+    console.log("startX", startX);
+    startY = e.clientY;
+    console.log("startY", startY);
+  });
 
-            canvas.addEventListener("mouseup" , (e) => {
-             clicked = false
-             console.log("up up up")
+  canvas.addEventListener("mouseup", (e) => {
+    clicked = false;
+    console.log("up up up");
+    const width = e.clientX - startX;
+    const height = e.clientY - startY;
+    existingShapes.push({
+        type:"rect",
+      x: startX,
+      y: startY,
+      width: width,
+      height: height
+    }) 
+  });
 
-            //  startX = e.clientX
-            //  startY= e.clientY
-            })
+  canvas.addEventListener("mousemove", (e) => {
+    if (clicked) {
+        console.log("mousemoove: " )
+      const width = e.clientX - startX;
+      const height = e.clientY - startY;
+      clearCanvas(existingShapes,canvas,ctx);
+      
 
-             canvas.addEventListener("mousemove" , (e) => {
-              
-                if(clicked){
-                    const width = e.clientX  - startX;
-                    const height = e.clientY - startY;
-               
-                     ctx.clearRect(0,0,canvas.width , canvas.height);
-                    ctx.strokeStyle ="white"
-                    ctx.strokeRect(startX,startY,width,height)
-                }
-            })
+      ctx.strokeStyle = "white";
+      ctx.strokeRect(startX, startY, width, height);
+    }
+  });
+}
 
+
+function clearCanvas(existingShapes:Shape[],canvas:HTMLCanvasElement,ctx:CanvasRenderingContext2D){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    ctx.fillStyle ="rgbs(0,0,0)",
+    ctx.fillRect(0,0,canvas.width,canvas.height)
+
+
+    existingShapes.map((shape) => {
+        if(shape.type === "rect"){
+          ctx.strokeStyle = "white";
+      ctx.strokeRect(shape.x, shape.y, shape.width, shape.height);
+        }
+    })
 }
